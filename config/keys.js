9dcdd66da18486f3b779fed9aa88de704821768f
Doctor-Phil0 <hash.munir123@gmail.com>
2022-05-07 20:56:39 +0500

    First Commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://Hash:123@cluster0-shard-00-00.8fo9t.mongodb.net:27017,cluster0-shard-00-01.8fo9t.mongodb.net:27017,cluster0-shard-00-02.8fo9t.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-10zas9-shard-0&authSource=admin&retryWrites=true&w=majority",
+  secretOrKey: "ba8c4b202d310d3d352fbde6176071e221e234ab910430c2a0e0cf0b0e63a98970c94c9"
+};
